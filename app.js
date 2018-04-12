6d37317cf6686d819670d5960c98a0d935c15471
Spencer York <spencerwyork@gmail.com>
2018-04-12 10:51:29 -0500

    users can create, update, and delete shows

--- app.js
+++ app.js
@@ -12,7 +12,25 @@ const { User, Show, Director } = app.get('models')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// SHOWS!
+// **** SHOWS ****
+
+// CREATE A SHOW
+app.post('/shows', (req, res, next) => {
+    Show.create({
+        name: req.body.name,
+        network: req.body.network,
+        genre: req.body.genre,
+        in_production: req.body.in_production,
+        directorId: req.body.directorId    
+    })
+    .then( (show) => {
+        res.status(200).json(show)
+    })
+    .catch( (error) => {
+        console.log('Show could not be added', error);
+        res.status(500).json(error);
+    })
+});
 
 // GET ALL SHOWS
 app.get('/shows', (req, res, next) => {
@@ -41,8 +59,23 @@ app.get('/shows/:id', (req, res, next) => {
     })
 });
 
+// UPDATE ONE SHOW
+app.put('/shows/:id', (req, res, next) => {
+    Show.update({
+        name: req.body.name,
+        network: req.body.network,
+        genre: req.body.genre,
+        in_production: req.body.in_production,
+        directorId: req.body.directorId 
+    }, {where: {id: req.params.id}})
+    .then( () => {
+        res.status(200).json();
+        console.log("Director has been updated");
+    });
+});
+
 // DELETE A SHOW
-app.delete('shows/:id', (req, res, next) => {
+app.delete('/shows/:id', (req, res, next) => {
     Show.destroy({
         where: {id: req.params.id }
     })
@@ -68,8 +101,19 @@ app.post('/favorites', ({body: { UserId, ShowId}}, res, next) => {
 
 // CREATE ONE DIRECTOR
 app.post('/directors', (req, res, next) => {
-    
-})
+    Director.create({
+        name: req.body.name,
+        birth_year: req.body.birth_year,
+        twitter_handle: req.body.twitter_handle
+    })
+    .then( director => {
+        res.status(200).json(director)
+    })
+    .catch( (error) => {
+        console.log("user could not be added", error)
+        res.status(500).json({error})
+    });
+});
 
 
 // GET ALL DIRECTORS
@@ -110,6 +154,19 @@ app.put('/directors/:id', (req, res, next) => {
     .then( () => {
         res.status(200).json();
         console.log("Director has been updated");
+    });
+});
+
+// DELETE ONE DIRECTOR
+app.delete('/directors/:id', (req, res, next) => {
+    console.log("delete is being called");
+    Director.destroy({
+        where: {id: req.params.id},
+        force: true
+    })
+    .catch( (err) => {
+        console.log("delete not complete", err);
+        res.status(500).json({error});
     });
 });
 
